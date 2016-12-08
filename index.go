b89fd75835d38dcb5501a3ef1695f30733ff9ff0
野口翔平 <ns.fresnel07@gmail.com>
2016-12-08 23:22:09 +0900

    goテスト用

---
+++ index.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "net/http"
+    "net/http/fcgi"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+    l, err := net.Listen("tcp", "127.0.0.1:9000")
+    if err != nil {
+        return
+    }
+    http.HandleFunc("/", handler)
+    fcgi.Serve(l, nil)
+}
